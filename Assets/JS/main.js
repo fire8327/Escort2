94d4478928a0ed739f04d4184e076174581619e8
fire8327 <fire83274@gmail.com>
2024-08-01 18:02:06 +0300

    create tabs

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -45,3 +45,25 @@ $("#filterToggler").click(() => {
   $("#filterToggler").find("img").toggleClass("rotate-180")
   $("#filterList").slideToggle()
 })
+
+
+/* tabs */
+$(".tab-main").each(function(index) {
+  let $this = $(this);
+  let prefix = "tabs-" + index;
+
+  $this.find("ul a").each(function(tabIndex) {
+    let $tab = $(this);
+    let tabId = prefix + "-tab-" + tabIndex;
+    let panelId = prefix + "-panel-" + tabIndex;
+
+    $tab.attr("href", "#" + panelId);
+    $tab.closest("li").attr("id", tabId);
+  });
+
+  $this.find(".tab-card").each(function(panelIndex) {
+    $(this).attr("id", prefix + "-panel-" + panelIndex);
+  });
+
+  $this.tabs();
+});
